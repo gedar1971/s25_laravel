b5ee872421acc4db1a835488e8577498c3507a59
gedar1971 <german.arbelaez@est.iudigital.edu.co>
2021-12-12 18:52:58 -0500

    s25act

--- resources/views/dashboard/category/create.blade.php
+++ resources/views/dashboard/category/create.blade.php
@@ -1,7 +1,11 @@
 @extends('dashboard.master')
 @section('content')
-    <h6>Crear Publicacion</h6>
-    <form action="{{ route('post.store') }}" method="POST"> 
+    <h6>Crear Categoria </h6>
+    <form action="{{ route('category.store') }}" method="POST"> 
             @include('dashboard.category._form')
     </form>
 @endsection
+
+
+
+
